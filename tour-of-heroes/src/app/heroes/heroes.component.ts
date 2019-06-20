b6ace8576990b473f90d26dcd89e70d70a490878
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-20 15:00:54 +0530

    service

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes'
+import {HeroService} from '../services/hero.service';
 
 @Component({
   selector: 'heroes',
@@ -9,14 +9,19 @@ import {HEROES} from '../mock-heroes'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero:Hero;
   details( hero: Hero ):void {
     this.selectedHero = hero;   
   }
-  constructor() { }
+  constructor( private heroservice: HeroService ) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes():void {
+    this.heroservice.getHeroes()
+    .subscribe( heroes=> this.heroes = heroes );
   }
 
 }
