05cb60202753f787053c34ae5766e9e2373cac02
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-20 12:59:04 +0530

    module

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './modules/material-ui/material-ui.module'
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
@@ -14,6 +16,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     FormsModule
   ],
   providers: [],
---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 101, name: 'Iron man' },
+    { id: 102, name: 'Nova' },
+    { id: 103, name: 'Captain Marvel' },
+    { id: 104, name: 'Spider man' },
+    { id: 105, name: 'Black Panther' },
+    { id: 106, name: 'The flash' },
+    { id: 107, name: 'Arrow' },
+    { id: 108, name: 'Shazam' },
+    { id: 109, name: 'Rhino' },
+    { id: 110, name: 'Ultron' },
+];
