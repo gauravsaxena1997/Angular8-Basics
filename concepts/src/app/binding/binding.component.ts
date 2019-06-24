0d1c0eb930f301ad667bdb53c853ca3b61523219
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-24 15:53:17 +0530

    style and class binding

--- concepts/src/app/binding/binding.component.ts
+++ concepts/src/app/binding/binding.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-binding',
   templateUrl: './binding.component.html',
@@ -7,8 +27,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingComponent implements OnInit {
   headerValue:string = 'Angular Bindings';
+  table_row = ELEMENT_DATA;
+  nineth_index:PeriodicElement = this.table_row[7];
+  firstElement:string = '';
   constructor() { }
 
+  settheadStyles(): {}{
+    let theadStyles = {
+      'backgroundColor':'lightblue',
+      'fontSize.px': '20',
+      'color':'white'
+    }
+    return theadStyles;
+  }
+  getFirstElement(){
+    this.firstElement =  this.table_row[0].name;
+    alert(this.firstElement);
+  }
   ngOnInit() {
   }
 
