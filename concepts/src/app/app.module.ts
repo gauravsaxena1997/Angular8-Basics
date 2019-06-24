6ba2713cb6470c6d7e678af97a4bd63eb4aa8261
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-24 20:31:40 +0530

    added angular material

--- concepts/src/app/app.module.ts
+++ concepts/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './modules/material-ui/material-ui.module';
 import { BindingComponent } from './binding/binding.component';
 import { ChildComponent } from './child/child.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { LifecycleHooksComponent } from './lifecycle-hooks/lifecycle-hooks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BindingComponent,
     ChildComponent,
-    UserInputComponent
+    UserInputComponent,
+    LifecycleHooksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
