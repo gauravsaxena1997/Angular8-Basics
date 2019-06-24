cb76375a7743edf9ed83df286c7e961f85eadf8f
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-24 19:49:46 +0530

    Get user input

--- concepts/src/app/user-input/user-input.component.ts
+++ concepts/src/app/user-input/user-input.component.ts
@@ -7,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInputComponent implements OnInit {
   clickCount:number = 0;
+  values:string = '';
+  values2:string = '';
+  upper:string = '';
+  val:string = '';
   count():void{
     this.clickCount++;
+  }
+  onkeyup(event:any){
+    this.values += event.key+ ' | ';
+    console.log(typeof(event.target.value));
+    
+  }
+  onkeyup2(event:KeyboardEvent){
+    this.values2 += (<HTMLInputElement>event.target).value+ ' | ';    
+  }
+  toUpper(val:string){
+    this.upper =  val.toUpperCase();
+  }
+  getval(val:string){
+    this.val = val;
   }
   constructor() { }
 
