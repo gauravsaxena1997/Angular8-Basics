55477b7cf7c2661df46eee14740949cdcaf8dc3a
gauravsaxena1997 <gauravsaxena.cs@gmail.com>
2019-07-04 15:10:42 +0530

    reactive programming

--- concepts/src/app/components/reactive-programming/reactive-programming.component.ts
+++ concepts/src/app/components/reactive-programming/reactive-programming.component.ts
@@ -1,9 +1,10 @@
-import { debounceTime, filter } from 'rxjs/operators';
+import { debounceTime, filter, map } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 import { BandDataService } from './band-data.service';
 import { Band } from './model';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-programming',
@@ -15,6 +16,13 @@ export class ReactiveProgrammingComponent implements OnInit {
   bandForm: FormGroup;
   readonly localStorageKey = 'band-crate-snapshot';
   constructor(fb: FormBuilder,private bandDataService: BandDataService) {
+    const obs = interval(1000).pipe(
+      map(val=>val*val),
+      filter(val=>val%2==0)
+    );
+    obs.subscribe(
+      (val)=> console.log(val)
+    )
     this.bandDataService.getBands().subscribe(bands => {
       this.bandList = bands;
     });
